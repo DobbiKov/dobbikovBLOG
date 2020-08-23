c83697875e7e6d7d78d72ab479365ed792e71542
DobbiKov <dobbikov@gmail.com>
2020-08-23 19:19:40 +0300

    added comments for posts

--- myblog/Data/Migrations/20200823132353_Init15.Designer.cs
+++ myblog/Data/Migrations/20200823132353_Init15.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using myblog.Data;
 
 namespace myblog.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200823132353_Init15")]
+    partial class Init15
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -482,9 +484,6 @@ namespace myblog.Data.Migrations
 
                     b.Property<DateTime>("Time")
                         .HasColumnType("datetime2");
-
-                    b.Property<string>("Token")
-                        .HasColumnType("nvarchar(max)");
 
                     b.Property<Guid>("UserId")
                         .HasColumnType("uniqueidentifier");
---
+++ myblog/Interfaces/IPostCommentsRepository.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using myblog.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace myblog.Interfaces
+{
+    public interface IPostCommentsRepository
+    {
+        IQueryable<PostComments> GetCommentsAsync(Guid postid);
+        Task<ActionResult<PostComments>> GetCommentAsync(Guid id);
+        Task Create(PostComments post);
+        Task<ActionResult<PostComments>> Update(PostComments post);
+        Task Delete(Guid id);
+        Task Init();
+    }
+}
